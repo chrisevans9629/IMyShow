059f425a401f1e9ef25595b68b4f3fb32adacca7
Chris Evans <chrisevans9629@gmail.com>
2021-04-03 21:34:39 -0500

    added icon and prompt

--- IMyShow/ViewModels/AboutViewModel.cs
+++ IMyShow/ViewModels/AboutViewModel.cs
@@ -1,10 +1,64 @@
 ﻿using System;
+using System.Timers;
 using System.Windows.Input;
 using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace IMyShow.ViewModels
 {
+    public class Prompts : Bindable
+    {
+        Timer timer = new Timer();
+        private string currentText;
+
+        public Prompts()
+        {
+            TextPrompts = new[]
+            {
+              "Welcome to my site!",
+              "I am a software developer",
+              "Console.WriteLine(\"My favorite language is C#\");",
+              "printfn \"But I also love F#\"",
+              "print(\"and python!\")",
+              "This site was built with jekyll...",
+              "and is hosted by github pages.",
+              "Hope you enjoy and please follow me on github!",
+            };
+            Delay = 1000;
+            timer.AutoReset = true;
+            timer.Interval = 100;
+            timer.Start();
+
+            timer.Elapsed += Timer_Elapsed;
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            var prompt = TextPrompts[PromptIndex];
+            if (TextIndex >= prompt.Length)
+            {
+                TextIndex = 0;
+                PromptIndex++;
+                CurrentText = "";
+                if (PromptIndex >= TextPrompts.Length)
+                {
+                    PromptIndex = 0;
+                }
+            }
+            CurrentText += TextPrompts[PromptIndex][TextIndex];
+            TextIndex++;
+        }
+        public int PromptIndex { get; set; }
+        public int TextIndex { get; set; }
+        public string CurrentText
+        {
+            get => currentText;
+            set => SetProperty(ref currentText, value);
+        }
+        public int Delay { get; set; }
+        public string[] TextPrompts { get; set; }
+    }
+
     public class AboutViewModel : BaseViewModel
     {
         public AboutViewModel()
@@ -12,7 +66,7 @@ namespace IMyShow.ViewModels
             Title = "About";
             OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamarin-quickstart"));
         }
-
+        public Prompts Prompts { get; set; } = new Prompts();
         public ICommand OpenWebCommand { get; }
     }
 }
--- IMyShow/Views/AboutPage.xaml.cs
+++ IMyShow/Views/AboutPage.xaml.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Linq;
+using System.Timers;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace IMyShow.Views
 {
+
     public partial class AboutPage : ContentPage
     {
         public AboutPage()
